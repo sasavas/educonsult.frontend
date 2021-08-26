9c72129d27026d350b5d425ba5a3ca36a3f91550
fsavas <savasusame@gmail.com>
2021-08-26 13:04:36 +0300

    feat impl dashboard

--- src/Modules/DashboardModule/Dashboard.js
+++ src/Modules/DashboardModule/Dashboard.js
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const Dashboard = () => {
+  const [counts, setCounts] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3008/dashboard").then((res) => {
+      setCounts(res.data);
+      console.log(res.data);
+    });
+  }, []);
+
   return (
     <div>
       <h2>Dashboard</h2>
+      {Object.keys(counts).map((key) => (
+        <p>
+          {key} {counts[key]}
+        </p>
+      ))}
     </div>
   );
 };
